037aa10f9df9d1be9810744d3956ff92fae496a0
silvio.lpires <silvio.lpires@sempreceub.com>
2025-04-29 10:43:21 -0300

    Configuração do admin para Trilhas

--- trilhas_view/admin.py
+++ trilhas_view/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Trilha
 
-# Register your models here.
+@admin.register(Trilha)
+class TrilhaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'distancia', 'duracao', 'nivel_dificuldade', 'data_criacao')
+    list_filter = ('nivel_dificuldade', 'data_criacao')
+    search_fields = ('nome', 'descricao')
+    filter_horizontal = ('guias',)  # Interface mais amigável para o relacionamento ManyToMany
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('nome', 'descricao', 'distancia', 'duracao', 'nivel_dificuldade')
+        }),
+        ('Guias', {
+            'fields': ('guias',)
+        }),
+    )
