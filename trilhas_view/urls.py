c35e02ea2fc6111e344176d19bb1d83b160f2307
silvio.lpires <silvio.lpires@sempreceub.com>
2025-04-29 11:21:10 -0300

    Implementação do CRUD completo de Trilhas

--- trilhas_view/urls.py
+++ trilhas_view/urls.py
@@ -6,4 +6,7 @@ app_name = 'trilhas_view'
 urlpatterns = [
     path('', views.lista_trilhas, name='lista_trilhas'),
     path('<int:trilha_id>/', views.detalhe_trilha, name='detalhe_trilha'),
+    path('criar/', views.criar_trilha, name='criar_trilha'),
+    path('<int:trilha_id>/editar/', views.editar_trilha, name='editar_trilha'),
+    path('<int:trilha_id>/excluir/', views.excluir_trilha, name='excluir_trilha'),
 ]
--- trilhas_view/views.py
+++ trilhas_view/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Trilha
+from .forms import TrilhaForm
 
 def lista_trilhas(request):
     trilhas = Trilha.objects.all()
@@ -8,3 +11,47 @@ def lista_trilhas(request):
 def detalhe_trilha(request, trilha_id):
     trilha = get_object_or_404(Trilha, pk=trilha_id)
     return render(request, 'trilhas_view/detalhe_trilha.html', {'trilha': trilha})
+
+def criar_trilha(request):
+    if request.method == 'POST':
+        form = TrilhaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Trilha criada com sucesso!')
+            return redirect('trilhas_view:lista_trilhas')
+    else:
+        form = TrilhaForm()
+    
+    return render(request, 'trilhas_view/form_trilha.html', {
+        'form': form,
+        'titulo': 'Nova Trilha',
+        'botao': 'Criar'
+    })
+
+def editar_trilha(request, trilha_id):
+    trilha = get_object_or_404(Trilha, pk=trilha_id)
+    
+    if request.method == 'POST':
+        form = TrilhaForm(request.POST, instance=trilha)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Trilha atualizada com sucesso!')
+            return redirect('trilhas_view:detalhe_trilha', trilha_id=trilha.id)
+    else:
+        form = TrilhaForm(instance=trilha)
+    
+    return render(request, 'trilhas_view/form_trilha.html', {
+        'form': form,
+        'titulo': f'Editar Trilha: {trilha.nome}',
+        'botao': 'Atualizar'
+    })
+
+def excluir_trilha(request, trilha_id):
+    trilha = get_object_or_404(Trilha, pk=trilha_id)
+    
+    if request.method == 'POST':
+        trilha.delete()
+        messages.success(request, 'Trilha excluída com sucesso!')
+        return redirect('trilhas_view:lista_trilhas')
+    
+    return render(request, 'trilhas_view/confirmar_exclusao.html', {'trilha': trilha})
